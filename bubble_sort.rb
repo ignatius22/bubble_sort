75caa2880dd6f8358caca277b3ac2a423526e1c3
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-06-17 19:25:42 +0530

    Add unless instead of if

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -4,12 +4,13 @@ def bubble_sort(array)
   loop do
     swapped = false
 	(n-1).times do |i|
-	  if array[i] > array[i + 1]
-        array[i],array[i + 1] = array[i + 1],array[i]
-		swapped = true
-	  end
-	end
-    break if not swapped
+	  unless array[i] <= array[i + 1]
+
+      array[i], array[i + 1] = array[i + 1], array[i]
+      swapped = true
+      end
+    end
+    break if !swapped
   end
   array
 end
@@ -17,10 +18,10 @@ end
 def bubble_sort_by(arr)
   swapped = true
   n = arr.length
-  (0...n).each do |i|
+  (0...n).each do
     (0...n - 1).each do |b|
       next unless (yield arr[b], arr[b + 1]).positive?
-      
+
       temp = arr[b]
       arr[b] = arr[b + 1]
       arr[b + 1] = temp
@@ -31,7 +32,7 @@ def bubble_sort_by(arr)
   arr
 end
 
-bubsort_by = bubble_sort_by(['hello', 'hey', 'hi', 'here']) do |left, right|
+bubsort_by = bubble_sort_by(%w[hello hey hi here]) do |left, right|
   left.length - right.length
 end
 
